7b2d32f20ec91db81951771ec4bd8155bc4a6542
t <tmont091@gmail.com>
2025-11-13 15:37:56 -0800

    Initial Commit

---
+++ Enhanced Demo_SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Enhanced Demo_SwiftData
+//
+//  Created by Paradis d'Abbadon on 13.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Enhanced Demo_SwiftData/Enhanced_Demo_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enhanced_Demo_SwiftDataApp.swift
+//  Enhanced Demo_SwiftData
+//
+//  Created by Paradis d'Abbadon on 13.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Enhanced_Demo_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
